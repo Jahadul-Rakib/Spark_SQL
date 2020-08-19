5357a342b2d6d7cde3d0ddc5eda3cbd9fbf160b3
rakib <jhadul.islam@itconquest.com>
2020-08-19 15:37:34 +0600

    make temporary view dataset

--- src/main/java/com/rakib/App.java
+++ src/main/java/com/rakib/App.java
@@ -18,6 +18,14 @@ public class App {
                 .config("spark.sql.warehouse.dir", "file:///c:/temp/")
                 .getOrCreate();
 
+        Dataset<Row> dataset = session.read().option("header", true).csv("src/main/resources/student.csv");
+        //Its send the data to session and then perform sql into session
+        dataset.createOrReplaceTempView("my_students");
+
+        Dataset<Row> rowDataset = session.sql("select * from my_students order by Subject");
+        rowDataset.show();
+        Dataset<Row> rowDataset1 = session.sql("select avg(Score) from my_students");
+        rowDataset1.show();
 
 
         session.close();
