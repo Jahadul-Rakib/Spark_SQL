ea81f1fe1023b0c479628335f5ba22926ae2d1e5
rakib <jhadul.islam@itconquest.com>
2020-08-19 13:11:25 +0600

    add to up Stream

--- src/main/java/com/rakib/App.java
+++ src/main/java/com/rakib/App.java
@@ -1,6 +1,7 @@
 package com.rakib;
 
 
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -19,16 +20,14 @@ public class App {
                 .getOrCreate();
 
         Dataset<Row> dataSet = session.read().option("header", true).csv("src/main/resources/student.csv");
-        dataSet.createOrReplaceGlobalTempView("Student_Table");
 
-        //single condition expression
-        Dataset<Row> rowData = dataSet.filter("Subject = 'IT'");
-        rowData.show();
-
-        //multiple condition expression
-        Dataset<Row> dataset = dataSet.filter("Subject = 'Eng' AND Score <= 65");
-        dataset.show();
-
+        //single expression by lamda
+        Dataset<Row> rowDataset = dataSet.filter((FilterFunction<Row>) row -> row.getAs("Subject").equals("IT"));
+        rowDataset.show();
+        //multiple expression by lamda
+        Dataset<Row> rowDataset1 = dataSet.filter((FilterFunction<Row>) row -> row.getAs("Subject").equals("IT")
+                && Integer.parseInt(row.getAs("Score")) > 40);
+        rowDataset1.show();
 
 
         session.close();
