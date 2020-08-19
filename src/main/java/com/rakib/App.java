6b31cbc448554bdd23ee9aa1da1d0920eff09032
rakib <jhadul.islam@itconquest.com>
2020-08-19 16:03:14 +0600

    in memory Dataset Create

--- src/main/java/com/rakib/App.java
+++ src/main/java/com/rakib/App.java
@@ -1,10 +1,19 @@
 package com.rakib;
 
 
+import com.sun.prism.PixelFormat;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,14 +27,26 @@ public class App {
                 .config("spark.sql.warehouse.dir", "file:///c:/temp/")
                 .getOrCreate();
 
-        Dataset<Row> dataset = session.read().option("header", true).csv("src/main/resources/student.csv");
-        //Its send the data to session and then perform sql into session
-        dataset.createOrReplaceTempView("my_students");
+        List<Row> rowList = new ArrayList<>();
+        rowList.add(RowFactory.create(1, "Rakib7", 28, "IT"));
+        rowList.add(RowFactory.create(2, "Rakib1", 23, "Eng"));
+        rowList.add(RowFactory.create(3, "Rakib2", 25, "Ban"));
+        rowList.add(RowFactory.create(4, "Rakib3", 22, "SS"));
+        rowList.add(RowFactory.create(5, "Rakib4", 21, "EEE"));
+        rowList.add(RowFactory.create(6, "Rakib5", 20, "CSE"));
+        rowList.add(RowFactory.create(7, "Rakib6", 29, "Math"));
 
-        Dataset<Row> rowDataset = session.sql("select * from my_students order by Subject");
-        rowDataset.show();
-        Dataset<Row> rowDataset1 = session.sql("select avg(Score) from my_students");
-        rowDataset1.show();
+        //It will take Object Type of StructType
+        StructField[] fields = new StructField[]{
+                new StructField("Id", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("Name", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("Age", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("Department", DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType shema = new StructType(fields);
+        Dataset<Row> dataset = session.createDataFrame(rowList, shema);
+        dataset.show();
 
 
         session.close();
